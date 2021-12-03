237accbbe11fb34e93377ea296d7a8522c33f6a8
AurelioGon <agonzalez@sisu.mx>
2021-12-03 15:11:17 -0600

    Vercion terminada raw

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express")
+const cors = require("cors")
 const fileUpload = require('express-fileupload')
 const app = express()
 const routesServices = require("./routes/routerHub")
-
+const morgan = require("morgan")
+app.use(morgan("dev"))
+app.use(cors())
 app.use(fileUpload())
-
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 app.use(routesServices)
 
 module.exports = app
--- src/services/image/ImageControllers.js
+++ src/services/image/ImageControllers.js
@@ -1,10 +1,27 @@
 const { getTextByImage } = require("./ImageDto.js") 
-
+const template = (text) => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style>
+            .title{
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1 class="title">Se reconocio tu Texto: </h1>
+        <p class="text">${text}</p>
+    </body>
+    </html>`
+}
 const imageocrController = async(req,res) => {
     const text = await getTextByImage(req.files.file)
-    res.json({
-        text
-    })
+    res.status(400).send(template(text))
 }
 module.exports = {
     imageocrController
