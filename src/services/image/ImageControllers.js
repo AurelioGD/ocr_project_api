b911df88f6afbcbff218d1feda88a4916d4730c7
AurelioGon <agonzalez@sisu.mx>
2021-12-07 00:57:33 -0600

    Se retoco el diseño de la respuesta

--- src/services/image/ImageControllers.js
+++ src/services/image/ImageControllers.js
@@ -7,15 +7,25 @@ const template = (text) => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
         <style>
+            body{
+                display:flex;
+                justify-content:center;
+            }
             .title{
                 text-align: center;
+            }
+            .container{
+                width:60%;
             }
         </style>
     </head>
     <body>
-        <h1 class="title">Se reconocio tu Texto: </h1>
-        <p class="text">${text}</p>
+        <div class="container">
+            <h1 class="title text-3xl">Se reconocio tu Texto: </h1>
+            <p class="text">${text}</p>
+        </div>
     </body>
     </html>`
 }
